ef15f5a28d8470ce73d92cf92b5d7b50d1f1a25b
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-12-05 15:19:30 +0530

    Update Pritunl-Client-Ubuntu.sh

--- Pritunl-Client-Ubuntu.sh
+++ Pritunl-Client-Ubuntu.sh
@@ -8,3 +8,4 @@ gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo tee /etc/apt/trusted.gpg.d/pritunl.asc
 sudo apt update
 sudo apt install pritunl-client-electron
+pritunl-client-electron
