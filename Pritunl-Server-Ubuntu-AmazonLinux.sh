4996e7c9cdb018d7048e65f4f0dbc472eb23ea07
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-12-05 11:16:57 +0530

    Update Pritunl-Server-Ubuntu-AmazonLinux.sh

--- Pritunl-Server-Ubuntu-AmazonLinux.sh
+++ Pritunl-Server-Ubuntu-AmazonLinux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Function to detect the operating system
 get_os() {
@@ -13,9 +13,9 @@ get_os() {
 
 # Function to install dependencies and Pritunl
 install_pritunl() {
-    if [ "$1" == "amazon-linux" ]; then
+    if [ "$1" = "amazon-linux" ]; then
         # Amazon Linux setup
-        sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo << EOF
+        sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo << 'EOF'
 [mongodb-org-6.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/6.0/x86_64/
@@ -49,7 +49,7 @@ EOF
         sudo systemctl enable mongod pritunl
         sudo systemctl start mongod pritunl
 
-    elif [ "$1" == "ubuntu" ]; then
+    elif [ "$1" = "ubuntu" ]; then
         # Ubuntu setup
         sudo tee /etc/apt/sources.list.d/pritunl.list << 'EOF'
 deb http://repo.pritunl.com/stable/apt jammy main
